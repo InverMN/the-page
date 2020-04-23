45d5c03bb542317e98655a5b8802c4bdd782e9b4
Pawel <jankowskipawelxd@gmail.com>
2020-04-23 21:42:11 +0200

    Add input time of trigger value

--- addons.js
+++ addons.js
@@ -1,8 +1,8 @@
 $(() => {
 	let today = new Date()
-	let trigger = new Date(2020, 3, 24, 4, 0, 0, 0)
+	let trigger =  $('meta[id]').attr('id')
 	if(today > trigger) {
-		$('main').hide()
+		$('body > *').hide()
 		$('body').append('<iframe style="height: 100vh; width: 100%;" src="https://www.youtube.com/embed/Ct6BUPvE2sM?start=4&autoplay=1&controls=0&rel=0&modestbranding=1&loop=1&showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>')
 	}
 })
