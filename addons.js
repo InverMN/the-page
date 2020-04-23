46b2b861be8db4d9c3dc3e13546db59059dba5be
Pawel <jankowskipawelxd@gmail.com>
2020-04-23 22:00:05 +0200

    Add control

--- addons.js
+++ addons.js
@@ -1,8 +1,10 @@
 $(() => {
-	let today = new Date()
-	let trigger =  $('meta[id]').attr('id')
-	if(today > trigger) {
+	let trigger =  $('meta[id]').attr('id') || $('meta[update]').attr('update')
+	let video = $('meta[code]').attr('code') || 'Ct6BUPvE2sM'	
+	let start = $('meta[version]').attr('version') || 0 
+	if(new Date() > new Date(trigger)) {
+		console.log('test')
 		$('body > *').hide()
-		$('body').append('<iframe style="height: 100vh; width: 100%;" src="https://www.youtube.com/embed/Ct6BUPvE2sM?start=4&autoplay=1&controls=0&rel=0&modestbranding=1&loop=1&showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>')
+		$('body').append(`<iframe style="height: 100vh; width: 100%;" src="https://www.youtube.com/embed/${video}?start=${start}&autoplay=1&controls=0&rel=0&modestbranding=1&loop=1&showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>`)
 	}
 })
