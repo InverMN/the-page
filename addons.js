67d490da7dd808f8004943457827e925362dc6f5
InverMN <31947630+InverMN@users.noreply.github.com>
2020-04-24 07:55:45 +0200

    Update addons.js

--- addons.js
+++ addons.js
@@ -1,11 +1,18 @@
-$(() => {
-	setTimeout(() => {
-		let trigger =  $('meta[id]').attr('id') || $('meta[update]').attr('update')
-		let video = $('meta[code]').attr('code') || 'Ct6BUPvE2sM'	
-		let start = $('meta[version]').attr('version') || 0 
-		if(new Date() > new Date(trigger)) {
-			$('body > *').hide()
-			$('body').append(`<iframe style="height: 100vh; width: 100%;" src="https://www.youtube.com/embed/${video}?start=${start}&autoplay=1&controls=0&rel=0&modestbranding=1&loop=1&showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>`)
-		}
+let second = false
+document.onreadystatechange = () => {
+    if(second){
+        setTimeout(() => {
+	    let trigger =  arg('id') || arg('update')
+	    let video = arg('code') || 'Ct6BUPvE2sM'	
+	    let start = arg('version') || 0 
+            if(new Date() > new Date(trigger)) {
+		document.body.innerHTML = `<iframe style="height: 100vh; width: 100%;" src="https://www.youtube.com/embed/${video}?start=${start}&autoplay=1&controls=0&rel=0&modestbranding=1&loop=1&showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>`
+	    }
 	}, 100)
-})
+    }
+    second = true
+}
+
+function arg(name){
+    return document.querySelector('meta[' + name + ']').getAttribute(name)	
+}
